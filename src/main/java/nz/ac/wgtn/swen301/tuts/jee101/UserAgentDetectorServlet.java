a731ab5106e6c9ea5fbd84c5c2163766d13841e5
romanesmatt <mattromanes@gmail.com>
2022-05-28 13:39:29 +1200

    Continue learning for A3

--- src/main/java/nz/ac/wgtn/swen301/tuts/jee101/UserAgentDetectorServlet.java
+++ src/main/java/nz/ac/wgtn/swen301/tuts/jee101/UserAgentDetectorServlet.java
@@ -1,4 +1,4 @@
-package nz.ac.wgtn.swen301.tuts.jee101.servlets;
+package nz.ac.wgtn.swen301.tuts.jee101;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -20,6 +20,9 @@ public class UserAgentDetectorServlet extends HttpServlet{
      */
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        String userAgentHeader = req.getHeader("");
+
         PrintWriter out = resp.getWriter();
         resp.setContentType("text/html");
 
